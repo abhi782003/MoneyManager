d508b4d29cd23dce20c0601f104eed72081caa03
abhi2003 <tapsaleabhijeet@gmail.com>
2026-01-11 22:59:31 +0530

    Fix income/expense totals and add dashboard & filter features

---
+++ src/main/java/in/abhijeet/moneymanager/controller/DashboardController.java
@@ -0,0 +1,25 @@
+package in.abhijeet.moneymanager.controller;
+
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import in.abhijeet.moneymanager.service.DashboardService;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/dashboard")
+public class DashboardController {
+
+	private final DashboardService dashboardService;
+	
+	@GetMapping
+	public ResponseEntity<Map<String, Object>> getDashboardData(){
+		Map<String, Object> dashboardData = dashboardService.getDashboardData();
+        return ResponseEntity.ok(dashboardData);
+	}
+}
--- src/main/java/in/abhijeet/moneymanager/controller/IncomeController.java
+++ src/main/java/in/abhijeet/moneymanager/controller/IncomeController.java
@@ -1,11 +1,18 @@
 package in.abhijeet.moneymanager.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import in.abhijeet.moneymanager.dto.ExpenseDTO;
 import in.abhijeet.moneymanager.dto.IncomeDTO;
 import in.abhijeet.moneymanager.service.IncomeService;
 import lombok.RequiredArgsConstructor;
@@ -21,5 +28,17 @@ private final IncomeService incomeService;
 	public ResponseEntity<IncomeDTO> addIncome(@RequestBody IncomeDTO dto){
 		IncomeDTO saved = incomeService.addIncome(dto);
 		return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<IncomeDTO>> getExpenses(){
+		List<IncomeDTO> incomes = incomeService.getCurrentMonthIncomesForCurrentUser();
+		return ResponseEntity.ok(incomes); 
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteIncome(@PathVariable Long id){
+		incomeService.deleteIncome(id);
+		return ResponseEntity.noContent().build();
 	}
 }
